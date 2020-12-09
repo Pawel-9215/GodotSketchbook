10ce336834756338132a042aa5022f2f3b60e8f9
Pawel-9215 <hordyniak@gmail.com>
2020-12-09 18:57:19 +0100

    third test commit

---
+++ Node.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var firt = "What are we waiting for?"
+var due = "Godot I guess?"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(firt, due)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
